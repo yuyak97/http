a4404ca3c7d4ee9ccf5509e2ab9e0b93c26e6f6d
Yuya Kayama <yuya.kayama97@gmail.com>
2022-10-09 00:22:04 +0900

    add: goroutine化

--- socket/server/main.go
+++ socket/server/main.go
@@ -9,33 +9,42 @@ import (
 )
 
 func main() {
-	// ①ソケットの作成とIP:portのbind
+    goroutinSocket()
+}
+
+func goroutinSocket() {
+    // ①ソケットの作成とIP:portのbind
 	service := ":7777"
-	// net.ResolveTCPAddr("tcp4", service)の最初の引数は'tcp4', 'tcp6', 'tcp'が設定できます。これはIPv4, IPv6, IPv4orIPv6のどれを使うかを指定しています。
 	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
 	checkError(err)
-	// ②接続の待機
+
+    // ②接続の待機
 	listener, err := net.ListenTCP("tcp", tcpAddr)
 	checkError(err)
-	log.Println("normal socket\nlisten on port", service)
+	log.Println("concurrency socket\nlisten on port", service)
 	for {
         // ③接続の受信
-        conn, err := listener.Accept()
-        if err != nil {
-            log.Println(err)
-            continue
-        }
-        // ④ソケットの読み込み
-        req := make([]byte, 1024)
-        len, err := conn.Read(req)
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
 
-        log.Println("request:", string(req[:len])) // 部分配列　req[:len]
-        // ⑤ソケットの書き込み
-        daytime := time.Now().String()
-        conn.Write([]byte(daytime))
-        // ⑥接続の切断
-        conn.Close()
-    }
+        // 複数のクライアントからの接続を捌く必要があるので並行処理
+		go handleClient(conn)
+	}
+}
+
+func handleClient(conn net.Conn) {
+    // ⑥接続の切断
+	defer conn.Close()
+    // ④ソケットの読み込み
+    req := make([]byte, 1024)
+    len, err := conn.Read(req)
+    checkError(err)
+    log.Println("request:", string(req[:len])) // 部分配列　req[:len]
+    // ⑤ソケットの書き込み
+	daytime := time.Now().String()
+	conn.Write([]byte(daytime))
 }
 
 func checkError(err error) {
