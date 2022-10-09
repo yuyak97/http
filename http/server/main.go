b9c490cd8b13f5253b3f0cfcf1a1b5c98be7cc67
Yuya Kayama <yuya.kayama97@gmail.com>
2022-10-09 20:56:10 +0900

    add: requestBody取得処理

--- http/server/main.go
+++ http/server/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"net"
+	"net/textproto"
 	"os"
-	"time"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -37,17 +41,51 @@ func goroutinSocket() {
 }
 
 func handleClient(conn net.Conn) {
+	fmt.Println(">>> start")
+
 	// ⑥接続の切断
 	defer conn.Close()
 
-	// ④ソケットの読み込み
-	req := make([]byte, 1024)
-	len, err := conn.Read(req)
-	checkError(err)
-	log.Println("request:", string(req[:len])) // 部分配列　req[:len]
-	// ⑤ソケットの書き込み
-	daytime := time.Now().String()
-	conn.Write([]byte(daytime))
+	reader := bufio.NewReader(conn)
+	scanner := textproto.NewReader(reader)
+	scanRequest(scanner, reader, conn)
+
+	fmt.Println("<<< end")
+}
+
+func scanRequest(scanner *textproto.Reader, reader *bufio.Reader, conn net.Conn) {
+	var contentLength int
+
+	// 一行ずつ処理する
+	for {
+		line, err := scanner.ReadLine()
+		if line == "" {
+			break
+		}
+		if err != nil {
+			checkError(err)
+		}
+
+		if strings.HasPrefix(line, "Content-Length") {
+			contentLength, err = strconv.Atoi(strings.TrimSpace(strings.Split(line, ":")[1]))
+			if err != nil {
+				checkError(err)
+			}
+		}
+		fmt.Println(line)
+	}
+
+	// リクエストボディ
+	buf := make([]byte, contentLength)
+	_, err := io.ReadFull(reader, buf)
+	if err != nil {
+
+	}
+	fmt.Println("BODY:", string(buf))
+
+	if err != nil {
+		checkError(err)
+	}
 }
 
 func checkError(err error) {
