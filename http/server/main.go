47562ce8dac325540f7d9a617610d5fddfbc8f4a
Yuya Kayama <yuya.kayama97@gmail.com>
2022-10-09 21:51:40 +0900

    add: created basic http server

--- http/server/main.go
+++ http/server/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -68,39 +67,46 @@ func scanRequest(scanner *textproto.Reader, reader *bufio.Reader, conn net.Conn)
 		if line == "" {
 			break
 		}
-		if err != nil {
-			checkError(err)
-		}
-
+		
+		checkError(err)
+	
 		if isFirst {
 			isFirst = false
 			headerLine := strings.Fields(line)
 			header["Method"] = headerLine[0]
 			header["Path"] = headerLine[1]
-			fmt.Println(method, path)
 			continue
 		}
 
 		headerFields := strings.SplitN(line, ": ", 2)
-        fmt.Printf("%s: %s\n", headerFields[0], headerFields[1])
         header[headerFields[0]] = headerFields[1]
 	}
 
-	method, ok := header["Method"]
-    if !ok {
-        checkError(errors.New("no method found"))
-    }
+	// リクエストを表示
+	for k, v := range header {
+		fmt.Printf("%v:%v\n", k, v)
+	}
+
+	method = header["Method"]
+	path = header["Path"]
+	
+	if method == "GET" && path == "/" {
+		io.WriteString(conn, "HTTP/1.1 200 OK\r\n")
+		io.WriteString(conn, "Content-Type: text/html\r\n")
+    	io.WriteString(conn, "\r\n")
+    	io.WriteString(conn, "<h1>Hello World!!</h1>")
+	}
 
 	if method == "POST" || method == "PUT" {
         len, err := strconv.Atoi(header["Content-Length"])
-        if err != nil {
-            checkError(err)
-        }
+       
+        checkError(err)
+        
         buf := make([]byte, len)
         _, err = io.ReadFull(reader, buf)
-        if err != nil {
-            checkError(err)
-        }
+        
+        checkError(err)
+        
         fmt.Println("BODY:", string(buf))
     }
 }
